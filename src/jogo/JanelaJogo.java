067f57bb53576fe3bea76a7604da3686b483b1b0
dinhujr <c.souza0804@gmail.com>
2014-10-15 22:16:11 -0300

    layout basico com imagens da tela de jogo

--- src/jogo/JanelaJogo.java
+++ src/jogo/JanelaJogo.java
@@ -5,34 +5,63 @@
  */
 package jogo;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
 
 /**
  *
  * @author AleGomes
  */
-public class JanelaJogo {
-
-    public void start(Stage primaryStage) {
+public class JanelaJogo extends Application
+{
+    private AnchorPane pane;
+    private ImageView gameImage;
+    private static Stage stage;
+    
+    @Override
+    public void start(Stage primaryStage) 
+    {
+        initComponents();
+        
+        Scene scene = new Scene(pane);
         primaryStage.setTitle("War UFF");
-
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-
-        Text scenetitle = new Text("Novo Jogo");
-        grid.add(scenetitle, 0, 0, 2, 1);
-        scenetitle.setId("Titulo");
-        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setResizable(false);
         primaryStage.setScene(scene);
-        //scene.getStylesheets().add(TelaInicial.class.getResource("TelaInicial.css").toExternalForm());
         primaryStage.show();
+        stage = primaryStage;
+        
+        initLayout();
+    }
+    
+    private void initComponents()
+    {
+        pane = new AnchorPane();
+        pane.setPrefSize(1000, 600);
+        
+        gameImage = new ImageView(new Image("resources/gameMap.jpg"));
+        gameImage.setFitHeight(400);
+        gameImage.setFitWidth(800);
+        
+        pane.getChildren().addAll(gameImage);
+    }
+    
+    private void initLayout()
+    {
+        gameImage.setLayoutX(10);
+        gameImage.setLayoutY(10);
+    }
+    
+    public static Stage getStage()
+    {
+        return stage;
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
     }
 }
