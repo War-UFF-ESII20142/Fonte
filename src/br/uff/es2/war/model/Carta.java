3e45dfc9bb02dc37b7c91c90991083543e2e752f
GomesAle <alexandregomes@outlook.com>
2014-11-01 18:12:06 -0200

    Janela cartas e classe Carta
    Em construção

--- src/br/uff/es2/war/model/Carta.java
+++ src/br/uff/es2/war/model/Carta.java
@@ -3,39 +3,164 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package br.uff.es2.war.model;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
 /**
  *
  * @author claudio
  */
 public class Carta {
-    private SimpleIntegerProperty id;
-    private SimpleStringProperty descricao;
-    
-    public Carta(int id, String descricao)
-    {
-        this.id = new SimpleIntegerProperty(id);
-        this.descricao = new SimpleStringProperty(descricao);
+//    private SimpleIntegerProperty id;
+//    private SimpleStringProperty descricao;
+//    
+//    public Carta(int id, String descricao)
+//    {
+//        this.id = new SimpleIntegerProperty(id);
+//        this.descricao = new SimpleStringProperty(descricao);
+//    }
+//
+//    public int getId() {
+//        return id.get();
+//    }
+//
+//    public void setId(int id) {
+//        this.id.set(id);
+//    }
+//
+//    public String getDescricao() {
+//        return descricao.get();
+//    }
+//
+//    public void setDescricao(String descricao) {
+//        this.descricao.set(descricao);
+//    }
+
+    private BooleanProperty escolhida;
+    private StringProperty continente;
+    private StringProperty pais;
+    private StringProperty forma;
+
+    Carta(boolean escolhida, String continente, String pais, String forma) {
+
+        this.escolhida = new SimpleBooleanProperty(escolhida);
+
+        this.continente = new SimpleStringProperty(continente);
+
+        this.pais = new SimpleStringProperty(pais);
+
+        this.forma = new SimpleStringProperty(forma);
+
+        this.escolhida.addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) {
+                //System.out.println("Marcou");
+            }
+        });
     }
 
-    public int getId() {
-        return id.get();
+    /**
+     * @return the escolhida
+     */
+    public boolean getEscolhida() {
+        return escolhida.get();
     }
 
-    public void setId(int id) {
-        this.id.set(id);
+    /**
+     * @param escolhida the escolhida to set
+     */
+    public void setEscolhida(boolean escolhida) {
+        this.escolhida.set(escolhida);
     }
 
-    public String getDescricao() {
-        return descricao.get();
+    /**
+     * @return the continente
+     */
+    public String getContinente() {
+        return continente.get();
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao.set(descricao);
+    /**
+     * @param continente the continente to set
+     */
+    public void setContinente(String continente) {
+        this.continente.set(continente);
     }
-    
+
+    /**
+     * @return the pais
+     */
+    public String getPais() {
+        return pais.get();
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais) {
+        this.continente.set(pais);
+    }
+
+    /**
+     * @return the forma
+     */
+    public String getForma() {
+        return forma.get();
+    }
+
+    /**
+     * @param forma the forma to set
+     */
+    public void setForma(String forma) {
+        this.forma.set(forma);
+    }
 }
+
+//package br.uff.es2.war.model;
+//
+//import javafx.beans.property.SimpleIntegerProperty;
+//import javafx.beans.property.SimpleStringProperty;
+//
+///**
+// *
+// * @author claudio
+// */
+//public class Carta {
+//    private SimpleIntegerProperty id;
+//    private SimpleStringProperty descricao;
+//    
+//    public Carta(int id, String descricao)
+//    {
+//        this.id = new SimpleIntegerProperty(id);
+//        this.descricao = new SimpleStringProperty(descricao);
+//    }
+//
+//    public int getId() {
+//        return id.get();
+//    }
+//
+//    public void setId(int id) {
+//        this.id.set(id);
+//    }
+//
+//    public String getDescricao() {
+//        return descricao.get();
+//    }
+//
+//    public void setDescricao(String descricao) {
+//        this.descricao.set(descricao);
+//    }
+//    
+//}
--- src/br/uff/es2/war/view/JanelaCartas.java
+++ src/br/uff/es2/war/view/JanelaCartas.java
@@ -1,104 +1,182 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.uff.es2.war.view;
 
+import br.uff.es2.war.WindowManager;
+import br.uff.es2.war.model.Carta;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
-/**
- *
- * @author AleGomes
- */
 public class JanelaCartas extends Application {
-    /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+    private ObservableList<Carta> olCarta;
     private AnchorPane pane;
-    private Stage stage;
-    private Label lbTitulo;
-    private Button btnOk;
     private Button btnTrocar;
-  
+    private Button btnVoltar;
+    private HBox horizontalButtonBox;
+    private TableView<Carta> tbCarta;
+    private TableColumn<Carta, String> tcContinente;
+    private TableColumn<Carta, String> tcPais;
+    private TableColumn<Carta, String> tcForma;
+    private TableColumn<Carta, Boolean> tcEscolhida;
+    private Stage stage;
+    private WindowManager windowController;
+    private ImageView imgLogo;
+
     @Override
-    public void start(Stage primaryStage) throws Exception 
-    {
-        initComponents();
-        stage = primaryStage;
-        
-        Scene scene = new Scene(pane);
-        //scene.getStylesheets().add("/stylesheet/JanelaNumeroTropas.css");
-        this.stage.setScene(scene);
-        
-        stage.show();
-        
+    public void start(Stage primaryStage) {
+        initComponenets();
+
+        this.stage = primaryStage;
+
+        primaryStage.setTitle("Troca de Cartas");
+        Scene scene = new Scene(pane, primaryStage.getWidth(), primaryStage.getHeight());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(" Troca de Cartas ");
+        primaryStage.show();
+
         initListeners();
         initLayout();
     }
-    
-    private void initComponents() 
-    {
+
+    private void initLayout() {
+        tbCarta.setLayoutX((pane.getWidth() - tbCarta.getWidth()) / 2);
+        tbCarta.setLayoutY(110);
+
+        horizontalButtonBox.setLayoutX((pane.getWidth() - horizontalButtonBox.getWidth()) / 2);
+        horizontalButtonBox.setLayoutY(pane.getHeight() - 40 - horizontalButtonBox.getHeight());
+    }
+
+    public ObservableList<Carta> getPlayerList() {
+        return olCarta;
+    }
+
+    private void initListeners() {
+        //Trocar cartas
+        btnTrocar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                trocarCartas();
+                //Pegar as cartas marcadas
+                //TrocarCartas();
+                //Aumentar o numero de tropas para o jogador atual
+            }
+        });
+
+        btnVoltar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                stage.close();
+                //windowController.startMainWindow(JanelaJogo.this);
+            }
+        });
+    }
+
+    private void initComponenets() {
+        olCarta = FXCollections.observableArrayList();
+
         pane = new AnchorPane();
-        pane.setPrefSize(200, 200);
+        pane.setPrefSize(800, 600);
         pane.getStyleClass().add("pane");
-        
-        lbTitulo = new Label();
-        lbTitulo.setText("Suas Cartas:");
-        
-        //Tabela com quadradinho de marcar;
-        
-        //Botao de fazer trocas
-        
-        btnOk = new Button("ok");
+
+        tbCarta = new TableView<>();
+        tbCarta.getStyleClass().add("tbCarta");
+        tbCarta.setMinWidth(500);
+        tbCarta.setItems(olCarta);
+        tbCarta.setEditable(true);
+
+        btnVoltar = new Button("Voltar");
+        btnVoltar.getStyleClass().add("button");
+
         btnTrocar = new Button("Trocar");
-        
-        pane.getChildren().addAll(btnTrocar,lbTitulo,btnOk);
+        btnTrocar.getStyleClass().add("button");
+
+        horizontalButtonBox = new HBox(15);
+        horizontalButtonBox.getChildren().addAll(btnVoltar, btnTrocar);
+
+        tcContinente = new TableColumn<>("Continente");
+        tcPais = new TableColumn<>("Pais");
+        tcForma = new TableColumn("Forma");
+        tcEscolhida = new TableColumn<>("Escolhido");
+
+        tcContinente.setCellValueFactory(new PropertyValueFactory<Carta, String>("continente"));
+        tcContinente.setPrefWidth(tbCarta.getMinWidth() / 3);
+
+        tcPais.setCellValueFactory(new PropertyValueFactory<Carta, String>("pais"));
+        tcPais.setPrefWidth(tbCarta.getMinWidth() / 3);
+
+        tcForma.setCellValueFactory(new PropertyValueFactory<Carta, String>("forma"));
+        tcForma.setPrefWidth(tbCarta.getMinWidth() / 3);
+
+        tcEscolhida.setCellValueFactory(new PropertyValueFactory<Carta, Boolean>("escolhido"));
+        tcEscolhida.setPrefWidth(tbCarta.getMinWidth() / 3);
+
+        tcEscolhida.setCellFactory(
+                new Callback<TableColumn<Carta, Boolean>, TableCell<Carta, Boolean>>() {
+                    public TableCell<Carta, Boolean> call(TableColumn<Carta, Boolean> p) {
+                        return new CheckBoxTableCell<Carta, Boolean>();
+                    }
+                });
+
+        tbCarta.getColumns().addAll(tcEscolhida, tcContinente, tcPais, tcForma);
+
+        //imgLogo = new ImageView(new Image("resources/war-uff.png"));
+        //pane.getChildren().addAll(imgLogo,tbCarta,horizontalButtonBox);
+        pane.getChildren().addAll(tbCarta, horizontalButtonBox);
     }
-    
-    private void initLayout() 
-    {
-        lbTitulo.setLayoutX( (pane.getWidth() - lbTitulo.getWidth())/2 );
-        lbTitulo.setLayoutY( 50 );
-        
-        btnOk.setLayoutX( (pane.getWidth() - btnOk.getWidth())/2 );
-        btnOk.setLayoutY(150);
-        btnTrocar.setLayoutX( (pane.getWidth() - btnOk.getWidth())/2 );
-        btnTrocar.setLayoutY(150);
-    }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    private void initListeners() 
-    {
-        btnOk.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) 
-            {
-                stage.close();
+    public class CheckBoxTableCell<S, T> extends TableCell<S, T> {
+
+        private final CheckBox checkBox;
+
+        //private ObservableValue<T> ov;
+
+        public CheckBoxTableCell() {
+            this.checkBox = new CheckBox();
+            this.checkBox.setAlignment(Pos.CENTER);
+            setAlignment(Pos.CENTER);
+            setGraphic(checkBox);
+        }
+
+        @Override
+        public void updateItem(T item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                setGraphic(checkBox);
             }
-        });
-        
-        btnTrocar.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) 
-            {
-                //Realizar troca
+        }
+    }
+
+    private void trocarCartas() {
+        //Se uma linha da tabela esta marcada, add a lista
+        ObservableList<Carta> data = tbCarta.getItems();
+        ObservableList<Carta> cartasATrocar = FXCollections.observableArrayList();
+        for (Carta item : data) {
+            if (item.getEscolhida() == true) {
+                cartasATrocar.add(item);
             }
-        });
+        }
     }
 }
